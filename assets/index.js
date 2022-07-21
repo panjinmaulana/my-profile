305616b9490217264249a78b0fb6aa9ca1115528
Panji Maulana <panjinmaulana@gmail.com>
2022-07-21 09:26:09 +0700

    change directory path folder images, style and file index.js

--- assets/index.js
+++ assets/index.js
@@ -95,7 +95,11 @@ async function writeLoop() {
 };
 
 function delay(ms) {
-    return new Promise((resolve) => { setTimeout(() => { resolve() }, ms) });
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, ms);
+    });
 };
 
 writeLoop();
